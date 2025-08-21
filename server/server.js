754850866e7bfd34ace8e6ce80b7de78f3599429
Yuvraj Sharma <jayesh2005sharma@gmail.com>
2025-08-21 17:20:16 +0530

    Frontend url

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import userRouter from "./routes/userRouter.js";
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB()
-const allowedOrigins = ['http://localhost:5173']
+const allowedOrigins = [process.env.FRONTEND_URL]
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({origin: allowedOrigins ,credentials:true}));

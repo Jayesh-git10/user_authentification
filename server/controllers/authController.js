b0c70d36247555cb9409890f2d9382ffc1b9adb5
Yuvraj Sharma <jayesh2005sharma@gmail.com>
2025-08-30 18:04:30 +0530

    cookie changes to none

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -27,7 +27,7 @@ export const register = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -71,7 +71,7 @@ export const login = async (req, res) => {
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -87,7 +87,7 @@ export const logout = async (req, res) => {
         res.clearCookie('token', {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            sameSite: process.env.NODE_ENV === 'none',
         });
         return res.json({ success: true, message: "Logged Out" });
     } catch (error) {
